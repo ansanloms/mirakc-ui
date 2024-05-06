3ac2f84f797397b76038aea8630595db0d0248b6
ansanloms <ansanloms@users.noreply.github.com>
2024-05-06 15:03:53 +0900

    refactor

--- routes/api/mirakc/[...path].ts
+++ routes/api/mirakc/[...path].ts
@@ -1,31 +1,13 @@
 import { Handler, Handlers } from "$fresh/server.ts";
 
 const proxy: Handler = async (req) => {
-  const mirakcApiUrl = new URL(Deno.env.get("MIRAKC_API_URL") || "");
-
-  const proxyUrl = new URL(req.url);
-  proxyUrl.host = mirakcApiUrl.host;
-  proxyUrl.pathname = proxyUrl.pathname.replace(
+  const proxyUrl = new URL(Deno.env.get("MIRAKC_API_URL") || "");
+  proxyUrl.pathname = (new URL(req.url)).pathname.replace(
     /^(\/api\/mirakc)/,
-    mirakcApiUrl.pathname,
+    proxyUrl.pathname,
   );
 
-  const proxyReq = new Request(proxyUrl, {
-    method: req.method,
-    headers: req.headers,
-    body: req.body,
-    mode: req.mode,
-    credentials: req.credentials,
-    cache: req.cache,
-    redirect: req.redirect,
-    referrer: req.referrer,
-    referrerPolicy: req.referrerPolicy,
-    integrity: req.integrity,
-    keepalive: req.keepalive,
-    signal: req.signal,
-  });
-
-  const response = await fetch(proxyReq);
+  const response = await fetch(new Request(proxyUrl, req));
 
   return new Response(response.body, {
     status: response.status,
